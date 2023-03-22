eca7c10ea76ecf91e89089fe193647c826b875e1
andre simoes <andrevisimoes@gmail.com>
2023-03-22 16:49:00 -0300

    add responses

--- schemas.py
+++ schemas.py
@@ -2,3 +2,9 @@ from pydantic import BaseModel
 
 class UserCreateInput(BaseModel):
     name: str
+
+class StandardOutput(BaseModel):
+    message: str
+
+class AlternativeOutput(StandardOutput):
+    detail: str
--- views.py
+++ views.py
@@ -1,13 +1,15 @@
 from fastapi import APIRouter
+
 from services import UserService
-from schemas import UserCreateInput
+from schemas import UserCreateInput, StandardOutput, AlternativeOutput
 
 user_router = APIRouter(prefix='/user')
 assets_router = APIRouter(prefix='/assets')
 
-@user_router.post('/create')
+@user_router.post('/create', response_model=StandardOutput, responses={418: {'model': AlternativeOutput}})
 async def user_create(user_input: UserCreateInput):
     try:
         await UserService.create_user(name=user_input.name)
+        return StandardOutput(message='User created')
     except:
         pass
