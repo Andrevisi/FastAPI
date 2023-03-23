62dd81a310bc5aa7a008e9fe0de63ed09e636446
andre simoes <andrevisimoes@gmail.com>
2023-03-23 12:30:04 -0300

    remove favorite

--- views.py
+++ views.py
@@ -1,9 +1,7 @@
 from fastapi import APIRouter, HTTPException
 
-from services import UserService, FavoriteService
-from schemas import (
-    UserCreateInput, StandardOutput, ErrorOutoput, UserFavoriteAddInput
-)
+from services import *
+from schemas import *
 user_router = APIRouter(prefix='/user')
 assets_router = APIRouter(prefix='/assets')
 
@@ -27,6 +25,14 @@ async def user_delete(user_id: int):
 async def user_favorite_add(favorite_add: UserFavoriteAddInput):
     try:
         await FavoriteService.add_favorite(user_id=favorite_add.user_id, symbol=favorite_add.symbol)
+        return StandardOutput(message='OK')
+    except Exception as error:
+        raise HTTPException(status_code=400, detail=str(error))
+    
+@user_router.delete('/favorite/remove/{user_id}', response_model=StandardOutput, responses={400: {'model': ErrorOutoput}})
+async def user_favorite_remove(user_id: int, symbol: str):
+    try:
+        await FavoriteService.remove_favorite(user_id=user_id, symbol=symbol)
         return StandardOutput(message='OK')
     except Exception as error:
         raise HTTPException(status_code=400, detail=str(error))
