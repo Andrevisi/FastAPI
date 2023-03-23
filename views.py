64fae783af8bba42e2dd8e046068e4266592e85d
andre simoes <andrevisimoes@gmail.com>
2023-03-23 11:17:28 -0300

    param user_id altered

--- views.py
+++ views.py
@@ -13,3 +13,11 @@ async def user_create(user_input: UserCreateInput):
         return StandardOutput(message='OK')
     except Exception as error:
         raise HTTPException(status_code=400, detail=str(error))
+    
+@user_router.delete('/delete/{user_id}', response_model=StandardOutput, responses={400: {'model': ErrorOutoput}})
+async def user_delete(user_id: int):
+    try:
+        await UserService.delete_user(user_id)
+        return StandardOutput(message='OK')
+    except Exception as error:
+        raise HTTPException(status_code=400, detail=str(error))
